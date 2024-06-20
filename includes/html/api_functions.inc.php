b5cb0db3bf8c955b616021789eacd6cbcfd778dd
alfartZ <167224084+alfartZ@users.noreply.github.com>
2024-06-20 23:11:55 +0700

    Adding trigger auto discovery

--- includes/html/api_functions.inc.php
+++ includes/html/api_functions.inc.php
@@ -3926,3 +3926,59 @@ function show_graph(Request $request)
     }
 
 }
+
+function trigger_auto_discovery(Request $request)
+{
+    $vars = [
+        "device" => $request->query('device', 25),
+        "type" => $request->query('type', "discovery"),
+        "format" => $request->query('format', "text")
+    ]; 
+
+    $hostname = $vars['device'];
+    $type = $vars['type'];
+
+    switch ($type) {
+        case 'poller':
+            $cmd = ['php', \LibreNMS\Config::get('install_dir') . '/lnms', 'device:poll', $hostname, '--no-data', '-vv'];
+            $filename = "poller-$hostname.txt";
+            break;
+        case 'snmpwalk':
+            $device = device_by_name($hostname);
+
+            $cmd = gen_snmpwalk_cmd($device, '.', '-OUneb');
+
+            $filename = $device['os'] . '-' . $device['hostname'] . '.snmpwalk';
+            break;
+        case 'discovery':
+            $cmd = ['php', \LibreNMS\Config::get('install_dir') . '/discovery.php', '-h', $hostname, '-d'];
+            $filename = "discovery-$hostname.txt";
+            break;
+        default:
+            echo 'You must specify a valid type';
+            exit;
+    }
+
+    // ---- Output ----
+    $proc = new \Symfony\Component\Process\Process($cmd);
+    $proc->setTimeout(Config::get('snmp.exec_timeout', 1200));
+
+    if ($_GET['format'] == 'text') {
+        header('Content-type: text/plain');
+        header('X-Accel-Buffering: no');
+
+        $proc->run(function ($type, $buffer) {
+            echo preg_replace('/\033\[[\d;]+m/', '', $buffer) . PHP_EOL;
+            ob_flush();
+            flush(); // you have to flush buffer
+        });
+    } elseif ($_GET['format'] == 'download') {
+        $proc->run();
+        $output = $proc->getOutput();
+
+        $output = preg_replace('/\033\[[\d;]+m/', '', $output);
+
+        file_download($filename, $output);
+    }
+
+}
